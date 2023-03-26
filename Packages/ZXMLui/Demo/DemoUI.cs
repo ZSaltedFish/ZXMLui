7356c7cbd99aacc87aad9a6f1ada612372c80425
ZHANG JIAQING <a263382008@gmail.com>
2023-03-26 18:26:02 +0800

    DLL Reload功能

--- Packages/ZXMLui/Demo/DemoUI.cs
+++ Packages/ZXMLui/Demo/DemoUI.cs
@@ -10,9 +10,20 @@ namespace ZKnight.ZXMLui.Demo
         public EditorText TextInput;
         public EditorButton ClickBtn;
 
+        public override void Start()
+        {
+        }
+
+        public override void OnDllReloaded()
+        {
+            base.OnDllReloaded();
+            ReloadGUI();
+        }
+
         public void ClickBtn_OnBtnClick(EditorControl e)
         {
             Debug.Log(TextInput.Content);
+
         }
 
         [MenuItem("ZXMLui/Demo UI")]
